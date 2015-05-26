c0ee2fd666f2a11afd42e441131382953f40ffaa
yuma300 <ywakimoto2s@gmail.com>
2015-05-26 22:12:48 +0900

    support for balance api

---
+++ lib/ruby_coincheck_client/coincheck_client.rb
@@ -0,0 +1,33 @@
+require 'net/http'
+require 'uri'
+require 'openssl'
+
+class CoincheckClient
+  BASE_URL = "https://coincheck.jp/"
+  def read_balance(key, secret)
+    uri = URI.parse BASE_URL + "api/accounts/balance"
+    headers = get_signature(uri, key, secret)
+    request_for_get(uri, headers)
+  end
+
+  private
+    def request_for_get(uri, headers)
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      response = https.start {
+        https.get(uri.request_uri, headers)
+      }
+      puts response.body
+    end
+
+    def get_signature(uri, key, secret)
+      nonce = Time.now.to_i.to_s
+      message = nonce + uri.to_s
+      signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), secret, message)
+      headers = {
+        "ACCESS-KEY" => key,
+        "ACCESS-NONCE" => nonce,
+        "ACCESS-SIGNATURE" => signature
+      }
+    end
+end
--- ruby_coincheck_client.gemspec
+++ ruby_coincheck_client.gemspec
@@ -9,9 +9,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["yuma300"]
   spec.email         = ["ywakimoto2s@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{This is ruby client of coincheck api}
+  spec.homepage      = "https://github.com/yuma300/ruby-ripple-rest"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
   # delete this section to allow pushing this gem to any host.
