90ffea91d2aaa45e4b2d602f63d7509a865c29b1
yuma300 <ywakimoto2s@gmail.com>
2015-05-27 18:27:09 +0900

    support for order and send api

--- lib/ruby_coincheck_client/coincheck_client.rb
+++ lib/ruby_coincheck_client/coincheck_client.rb
@@ -1,48 +1,103 @@
 require 'net/http'
 require 'uri'
 require 'openssl'
+require 'json'
 
 class CoincheckClient
   BASE_URL = "https://coincheck.jp/"
-  def read_balance(key, secret)
+
+  def initialize(key, secret)
+    @key = key
+    @secret = secret
+  end
+
+  def read_balance
     uri = URI.parse BASE_URL + "api/accounts/balance"
-    headers = get_signature(uri, key, secret)
+    headers = get_signature(uri, @key, @secret)
     request_for_get(uri, headers)
   end
 
-  def read_accounts(key, secret)
+  def read_accounts
     uri = URI.parse BASE_URL + "api/accounts"
-    headers = get_signature(uri, key, secret)
+    headers = get_signature(uri, @key, @secret)
     request_for_get(uri, headers)
   end
 
-  def read_transactions(key, secret)
+  def read_transactions
     uri = URI.parse BASE_URL + "api/exchange/orders/transactions"
-    headers = get_signature(uri, key, secret)
+    headers = get_signature(uri, @key, @secret)
     request_for_get(uri, headers)
   end
 
-  def read_orders(key, secret)
+  def read_orders
     uri = URI.parse BASE_URL + "api/exchange/orders/opens"
-    headers = get_signature(uri, key, secret)
+    headers = get_signature(uri, @key, @secret)
     request_for_get(uri, headers)
+  end
+
+  def create_orders(rate, amount, order_type, pair = "btc_jpy")
+    body = {
+      rate: rate,
+      amount: amount,
+      order_type: order_type,
+      pair: pair
+    }
+    uri = URI.parse BASE_URL + "api/exchange/orders"
+    headers = get_signature(uri, @key, @secret, body.to_json)
+    request_for_post(uri, headers, body)
+  end
+
+  def delete_orders(id)
+    body = {
+      id: id,
+    }
+    uri = URI.parse BASE_URL + "api/exchange/orders/#{id}"
+    headers = get_signature(uri, @key, @secret)
+    request_for_delete(uri, headers)
+  end
+
+  def create_send_money(address, amount)
+    body = {
+      address: address,
+      amount: amount,
+    }
+    uri = URI.parse BASE_URL + "api/send_money"
+    headers = get_signature(uri, @key, @secret, body.to_json)
+    request_for_post(uri, headers, body)
   end
 
   private
-    def request_for_get(uri, headers)
+    def http_request(uri, request)
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
-      response = https.start {
-        https.get(uri.request_uri, headers)
-      }
-      puts response.body
+      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      response = https.start do |h|
+        h.request(request)
+      end
     end
 
-    def get_signature(uri, key, secret)
+    def request_for_get(uri, headers)
+      request = Net::HTTP::Get.new(uri.request_uri, initheader = headers)
+      http_request(uri, request)
+    end
+
+    def request_for_delete(uri, headers)
+      request = Net::HTTP::Delete.new(uri.request_uri, initheader = headers)
+      http_request(uri, request)
+    end
+    def request_for_post(uri, headers, body)
+      request = Net::HTTP::Post.new(uri.request_uri, initheader = headers)
+      request.body = body.to_json
+      http_request(uri, request)
+    end
+
+    def get_signature(uri, key, secret, body = "")
       nonce = Time.now.to_i.to_s
-      message = nonce + uri.to_s
+      message = nonce + uri.to_s + body
       signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), secret, message)
       headers = {
+        "Content-Type" => "application/json",
         "ACCESS-KEY" => key,
         "ACCESS-NONCE" => nonce,
         "ACCESS-SIGNATURE" => signature
