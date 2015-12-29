9dd071cb4d7e2a5d3609b08ac4ec97184e561d2e
Yutaro Iwamoto <dogyear.iwmt@gmail.com>
2015-12-29 17:10:45 +0900

    Update library

--- lib/ruby_coincheck_client/coincheck_client.rb
+++ lib/ruby_coincheck_client/coincheck_client.rb
@@ -24,6 +24,12 @@ class CoincheckClient
     request_for_get(uri, headers)
   end
 
+  def read_leverage_balance
+    uri = URI.parse @@base_url + "api/accounts/leverage_balance"
+    headers = get_signature(uri, @key, @secret)
+    request_for_get(uri, headers)
+  end
+
   def read_accounts
     uri = URI.parse @@base_url + "api/accounts"
     headers = get_signature(uri, @key, @secret)
@@ -36,17 +42,25 @@ class CoincheckClient
     request_for_get(uri, headers)
   end
 
+  def read_positions(body = {})
+    uri = URI.parse @@base_url + "api/exchange/leverage/positions"
+    headers = get_signature(uri, @key, @secret, body.to_json)
+    request_for_get(uri, headers, body)
+  end
+
   def read_orders
     uri = URI.parse @@base_url + "api/exchange/orders/opens"
     headers = get_signature(uri, @key, @secret)
     request_for_get(uri, headers)
   end
 
-  def create_orders(rate, amount, order_type, pair = "btc_jpy")
+  def create_orders(order_type:, rate: nil, amount: nil, market_buy_amount: nil, position_id: nil, pair: "btc_jpy")
     body = {
       rate: rate,
       amount: amount,
+      market_buy_amount: market_buy_amount,
       order_type: order_type,
+      position_id: position_id,
       pair: pair
     }
     uri = URI.parse @@base_url + "api/exchange/orders"
@@ -54,16 +68,13 @@ class CoincheckClient
     request_for_post(uri, headers, body)
   end
 
-  def delete_orders(id)
-    body = {
-      id: id,
-    }
+  def delete_orders(id: )
     uri = URI.parse @@base_url + "api/exchange/orders/#{id}"
     headers = get_signature(uri, @key, @secret)
     request_for_delete(uri, headers)
   end
 
-  def create_send_money(address, amount)
+  def create_send_money(address:, amount:)
     body = {
       address: address,
       amount: amount,
@@ -73,14 +84,119 @@ class CoincheckClient
     request_for_post(uri, headers, body)
   end
 
+  def read_send_money(currency: "BTC")
+    body = { currency: currency }
+    uri = URI.parse @@base_url + "api/send_money"
+    headers = get_signature(uri, @key, @secret, body.to_json)
+    request_for_get(uri, headers, body)
+  end
+
+  def read_deposit_money(currency: "BTC")
+    body = { currency: currency }
+    uri = URI.parse @@base_url + "api/deposit_money"
+    headers = get_signature(uri, @key, @secret, body.to_json)
+    request_for_get(uri, headers, body)
+  end
+
+  def create_deposit_money_fast(id: )
+    uri = URI.parse @@base_url + "api/deposit_money/#{id}/fast"
+    headers = get_signature(uri, @key, @secret)
+    request_for_delete(uri, headers)
+  end
+
   def read_ticker
     uri = URI.parse @@base_url + "api/ticker"
+    request_for_get(uri)
+  end
+
+  def read_trades
+    uri = URI.parse @@base_url + "api/trades"
     request_for_get(uri)
   end
 
   def read_order_books
     uri = URI.parse @@base_url + "api/order_books"
     request_for_get(uri)
+  end
+
+  def read_bank_accounts
+    uri = URI.parse @@base_url + "api/bank_accounts"
+    headers = get_signature(uri, @key, @secret)
+    request_for_get(uri, headers)
+  end
+
+  def create_bank_accounts(bank_name:, branch_name:, bank_account_type:, number:, name:)
+    body = {
+      bank_name: bank_name,
+      branch_name: branch_name,
+      bank_account_type: bank_account_type,
+      number: number,
+      name: name
+    }
+    uri = URI.parse @@base_url + "api/bank_accounts"
+    headers = get_signature(uri, @key, @secret, body.to_json)
+    request_for_post(uri, headers, body)
+  end
+
+  def delete_bank_accounts(id:)
+    uri = URI.parse @@base_url + "api/bank_accounts/#{id}"
+    headers = get_signature(uri, @key, @secret)
+    request_for_delete(uri, headers)
+  end
+
+  def read_withdraws
+    uri = URI.parse @@base_url + "api/withdraws"
+    headers = get_signature(uri, @key, @secret)
+    request_for_get(uri, headers)
+  end
+
+  def delete_withdraws(id:)
+    uri = URI.parse @@base_url + "api/withdraws/#{id}"
+    headers = get_signature(uri, @key, @secret)
+    request_for_delete(uri, headers)
+  end
+
+  def create_borrows(amount:, currency:)
+    body = {
+      amount: amount,
+      currency: currency
+    }
+    uri = URI.parse @@base_url + "api/lending/borrows"
+    headers = get_signature(uri, @key, @secret, body.to_json)
+    request_for_post(uri, headers, body)
+  end
+
+  def read_borrows
+    uri = URI.parse @@base_url + "api/lending/borrows/matches"
+    headers = get_signature(uri, @key, @secret)
+    request_for_get(uri, headers)
+  end
+
+  def delete_borrows(id:)
+    body = { id: id}
+    uri = URI.parse @@base_url + "api/lending/borrows/#{id}/repay"
+    headers = get_signature(uri, @key, @secret, body.to_json)
+    request_for_post(uri, headers, body)
+  end
+
+  def transfer_to_leverage(amount:, currency: "JPY")
+    body = {
+      amount: amount,
+      currency: currency
+    }
+    uri = URI.parse @@base_url + "api/exchange/transfers/to_leverage"
+    headers = get_signature(uri, @key, @secret, body.to_json)
+    request_for_post(uri, headers, body)
+  end
+
+  def transfer_from_leverage(amount:, currency: "JPY")
+    body = {
+      amount: amount,
+      currency: currency
+    }
+    uri = URI.parse @@base_url + "api/exchange/transfers/from_leverage"
+    headers = get_signature(uri, @key, @secret, body.to_json)
+    request_for_post(uri, headers, body)
   end
 
   private
@@ -96,8 +212,9 @@ class CoincheckClient
       end
     end
 
-    def request_for_get(uri, headers = {})
+    def request_for_get(uri, headers = {}, body = nil)
       request = Net::HTTP::Get.new(uri.request_uri, initheader = headers)
+      request.body = body.to_json if body
       http_request(uri, request)
     end
 
@@ -105,6 +222,7 @@ class CoincheckClient
       request = Net::HTTP::Delete.new(uri.request_uri, initheader = headers)
       http_request(uri, request)
     end
+
     def request_for_post(uri, headers, body)
       request = Net::HTTP::Post.new(uri.request_uri, initheader = headers)
       request.body = body.to_json
