a1004a1b19a6595de7c0804f302799d528808727
kdmsk <kudoma48@gmail.com>
2026-02-24 13:28:59 +0900

    Revert "Claude/add claude documentation o ap zj"

--- examples/private.rb
+++ examples/private.rb
@@ -4,7 +4,10 @@ Dotenv.load ".env"
 
 cc = CoincheckClient.new(ENV['API_KEY'], ENV['SECRET_KEY'])
 puts cc.read_balance.body
+puts cc.read_leverage_balance.body
 puts cc.read_accounts.body
 puts cc.read_transactions.body
+puts cc.read_positions(status: 'open').body
+puts cc.read_positions.body
 puts cc.read_trades().body
 puts cc.read_page_transactions.body
